bc5b39ba4082fc2b68878c2d33657d0f0977e42b
Sergio Rodriguez <SRodriguez@add123.com>
2020-06-14 23:26:30 -0400

    Initializing Blind Bag project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Bag from './bags/Bag'
+import ScrabbleBag from './bags/ScrabbleBag'
+import BananagramsBag from './bags/BananagramsBag'
+import SpellRPGBag from './bags/SpellRPGBag'
+
+export {
+  Bag,
+  ScrabbleBag,
+  BananagramsBag,
+  SpellRPGBag
+}
